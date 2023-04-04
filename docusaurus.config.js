9426f3b5e3cb85a529e2e8dbe68904acc842aae6
turinglabs <sebastiano.cataudo@gmail.com>
2023-04-04 10:03:18 +0200

    removing blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,19 +113,6 @@ const config = {
               },
             ],
           },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Niftyz.io`,
       },
